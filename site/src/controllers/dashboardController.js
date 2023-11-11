654cad3462c58725dc4517b968cc6f4a3a558ff0
zzzmiike <michael.silva@sptech.school>
2023-11-11 13:17:12 -0300

    machineData sendo zerada apos mostrar primeira maq

--- site/src/controllers/dashboardController.js
+++ site/src/controllers/dashboardController.js
@@ -124,5 +124,5 @@ module.exports = {
   listarAgenciasNOC,
   listarMaquinas,
   altoConsumoCPU,
-  altoConsumoRAM,
+  altoConsumoRAM
 }
--- site/src/models/dashboardModel.js
+++ site/src/models/dashboardModel.js
@@ -106,21 +106,11 @@ function altoConsumoRAM(banco) {
   return database.executar(instrucao);
 }
 
-function cadastrar(empresaId, descricao) {
-
-  instrucaoSql = `insert into (descricao, fk_empresa) aquario values (${descricao}, ${empresaId})`;
-
-  console.log("Executando a instrução SQL: \n" + instrucaoSql);
-  return database.executar(instrucaoSql);
-}
-
-
 module.exports = {
   listarMaquinasAg,
   listarAgenciasNOC,
   listarMaquinas,
   altoConsumoCPU,
   altoConsumoRAM,
-  statusMaquinas,
-  cadastrar
+  statusMaquinas
 }
